97b8534cee273e3398be7abf1e2b310870077429
benCombe <ben.combe29@gmail.com>
2025-01-28 16:41:56 -0500

    added AppDbContext and test controller

---
+++ Server/Models/TestItem.cs
@@ -0,0 +1,9 @@
+namespace Server.Models
+{
+    public class TestItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
