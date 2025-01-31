6ba1249f32904d720ad26bb5f4fa40ce4a872758
Spencer Ing <spencer.ing01@gmail.com>
2025-01-30 21:47:33 -0500

    Added User.cs to Server/Models

---
+++ Server/Models/User.cs
@@ -0,0 +1,49 @@
+public class User
+{
+    public string Id { get; set; } 
+
+    public string Type { get; set; }
+
+    public string Password { get; set; }
+
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }    
+    
+    public string Email { get; set; }   
+
+    public string Phone { get; set; }
+
+    public string Address { get; set; }
+
+    public string Region { get; set; }
+
+    public string PaymentInfo { get; set; }
+
+    public User(
+        string id,
+        string type,
+        string password,
+        string firstName,
+        string lastName,
+        string email,
+        string phone,
+        string address,
+        string region,
+        string paymentInfo
+    )
+    {
+        Id = id;
+        Type = type;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Phone = phone;
+        Address = address;
+        Region = region;
+        PaymentInfo = paymentInfo;
+    }
+        
+
+}
