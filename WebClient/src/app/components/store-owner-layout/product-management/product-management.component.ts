43b699047ea62d062f4538a6c2eaf9311d679521
Spencer Ing <spencer.ing01@gmail.com>
2025-03-18 00:17:20 -0400

    added passing for parent to child components

--- WebClient/src/app/components/store-owner-layout/product-management/product-management.component.ts
+++ WebClient/src/app/components/store-owner-layout/product-management/product-management.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
+import { NgFor, NgIf } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import { Item } from '../../../models/item';
+import { StoreDetails } from '../../../models/store-details';
+import { Category } from '../../../models/category';
+
+let ITEM_DATA: Item[] = [];
+let CAT_DATA: Category[] = [];
 
 @Component({
   selector: 'app-product-management',
-  imports: [],
+  imports: [NgFor, NgIf, MatTableModule],
   templateUrl: './product-management.component.html',
   styleUrl: './product-management.component.css'
 })
+
 export class ProductManagementComponent {
+    @Input() user: User | null | undefined;
+    @Input() storeDetails: StoreDetails | null | undefined;
+    @Input() productData = ITEM_DATA; // = Data for Product table 
+    @Input() categoryData = CAT_DATA; // Data for Category table
+    
+    displayedColumns: string[] = ['Id', 'Name', 'OriginalPrice', 'QuantityInStock', 'CategoryIds'];
 
 }
