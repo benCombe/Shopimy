3b7af8ee970112397367127cbadc73226f382cc6
steven <allmyuselessemails2@gmail.com>
2025-03-18 18:48:50 -0400

    version fixes

--- Server/Tests/WeatherForecastControllerTests.cs
+++ Server/Tests/WeatherForecastControllerTests.cs
@@ -25,16 +25,7 @@ public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactor
  
         var response = await _client.GetAsync("/weatherforecast");
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        *//*
-
+        */
     }
 
 }
-
-} //updating to test git
-
-
-//Commented out due to causing errors
-
-*/
-
