bd1fa036cf30ee40d108936237888f8c2424b4a6
Spencer Ing <spencer.ing01@gmail.com>
2025-03-17 23:52:43 -0400

    added ability to pass info to child components

--- WebClient/src/app/components/store-owner-layout/store-owner-dashboard/store-owner-dashboard.component.ts
+++ WebClient/src/app/components/store-owner-layout/store-owner-dashboard/store-owner-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { TopNavComponent } from "../../top-nav/top-nav.component";
 import { SideNavComponent } from "../side-nav/side-nav.component";
 import { NgClass, NgFor, NgIf } from '@angular/common';
@@ -12,6 +12,25 @@ import { OrdersComponent } from '../orders/orders.component';
 import { SettingsComponent } from '../settings/settings.component';
 import { AnalyticsComponent } from '../analytics/analytics.component';
 import { ThemesComponent } from '../themes/themes.component';
+import { SalesComponent } from '../sales/sales.component';
+import { StoreDetails } from '../../../models/store-details';
+import { StoreService } from '../../../services/store.service';
+import { DynamicTableComponent } from '../dynamic-table/dynamic-table.component';
+import { Item } from '../../../models/item';
+import { ItemService } from '../../../services/item.service';
+import { Category } from '../../../models/category';
+
+let ITEM_DATA: Item[] = [
+  {Id: '1', Name: 'Item 1', OriginalPrice: 100, SalePrice: 90, OnSale: true, Description: 'This is item 1', QuantityInStock: 10, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 5, CategoryIds: [1], ImageUrl: 'assets/images/default.png'},
+    {Id: '2', Name: 'Item 2', OriginalPrice: 200, SalePrice: 180, OnSale: true, Description: 'This is item 2', QuantityInStock: 20, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 4, CategoryIds: [2], ImageUrl: 'assets/images/default.png'},
+    {Id: '3', Name: 'Item 3', OriginalPrice: 300, SalePrice: 270, OnSale: true, Description: 'This is item 3', QuantityInStock: 30, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 3, CategoryIds: [3], ImageUrl: 'assets/images/default.png'},
+    {Id: '4', Name: 'Item 4', OriginalPrice: 500, SalePrice: 360, OnSale: true, Description: 'This is item 4', QuantityInStock: 40, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 2, CategoryIds: [4], ImageUrl: 'assets/images/default.png'},
+    {Id: '4', Name: 'Item 4', OriginalPrice: 500, SalePrice: 360, OnSale: true, Description: 'This is item 4', QuantityInStock: 40, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 2, CategoryIds: [4], ImageUrl: 'assets/images/default.png'},
+    {Id: '4', Name: 'Item 4', OriginalPrice: 500, SalePrice: 360, OnSale: true, Description: 'This is item 4', QuantityInStock: 40, AvailFrom: new Date(), AvailTo: new Date(), CurrentRating: 2, CategoryIds: [4], ImageUrl: 'assets/images/default.png'},
+  
+];
+let CAT_DATA: Category[] = [];
+
 
 
 @Component({
@@ -29,24 +48,45 @@ import { ThemesComponent } from '../themes/themes.component';
     OrdersComponent,
     SettingsComponent,
     AnalyticsComponent,
-    ThemesComponent
+    ThemesComponent,
+    SalesComponent,
+    DynamicTableComponent
   ],
   templateUrl: './store-owner-dashboard.component.html',
-  styleUrl: './store-owner-dashboard.component.css'
+  styleUrl: './store-owner-dashboard.component.css',
 })
+
+
 
 export class StoreOwnerDashboardComponent {
 
+    
   user: User | null | undefined;
-
+  storeDetails: StoreDetails | null | undefined;
+  item: Item | null | undefined;
+  
+  productData: Item[] = ITEM_DATA;
+  categoryData: Category[] = CAT_DATA;
   currentPage: string = "Overview"; //default page
 
-  constructor(private userService: UserService) {}
+
+  
+
+  constructor(private userService: UserService,
+              private storeService: StoreService) {}
 
   ngOnInit(){
     this.userService.activeUser$.subscribe(u =>{
       this.user = u;
-    })
+    });
+    
+    this.storeService.activeStore$.subscribe(sd =>{
+      this.storeDetails = sd;
+    });
+
+
+
+
   }
 
   onPageChange(newPage: string){
