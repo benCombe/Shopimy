a6fb824074fd750a678c6c6a6fa12710412dfdac
Spencer Ing <spencer.ing01@gmail.com>
2025-03-17 23:03:09 -0400

    added dynamic tables to orders

--- WebClient/src/app/components/store-owner-layout/orders/orders.component.ts
+++ WebClient/src/app/components/store-owner-layout/orders/orders.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-orders',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  totalOrdersColumns: string[] = ['totalSales', 'orders', 'customers', 'fulfilledOrders'];
+  orderDetailsColumns: string[] = ['order', 'customer', 'total', 'items', 'payment', 'fulfilled'];
+  
+  totalOrdersDataSource = new MatTableDataSource([
+    { totalSales: '$1500', orders: 16, customers: 5, fulfilledOrders: 1 }
+        // ... add other rows here
 
+  ]);
+
+  orderDetailsDataSource = new MatTableDataSource([
+    { order: '123456', customer: 'First Last', total: 50, items: 1, payment: 'Paid', fulfilled: 'Fulfilled' },
+    // ... add other rows here
+  ]);
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
